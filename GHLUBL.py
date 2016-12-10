b3df18b2dc188b9f27e98af7ff9efe05250271ca
jon85p <jhonvalencia314@gmail.com>
2016-12-10 11:43:52 -0500

    Autopep update

--- GHLUBL.py
+++ GHLUBL.py
@@ -6,13 +6,15 @@ import optparse
 import os
 import csv
 
-def find_between( s, first, last , starting=0):
+
+def find_between(s, first, last, starting=0):
     try:
-        start = s.index( first , starting) + len( first )
-        end = s.index( last, start )
+        start = s.index(first, starting) + len(first)
+        end = s.index(last, start)
         return s[start:end], start
     except ValueError:
         return ""
+
 
 def find_pages(s):
     '''
@@ -23,8 +25,9 @@ def find_pages(s):
     counter = 1
     while start != '>':
         start = s[last - counter]
-        counter+=1
+        counter += 1
     return int(s[last + 2 - counter:last])
+
 
 def find_users(htmls):
     '''
@@ -44,12 +47,13 @@ def find_users(htmls):
                 users.append(user)
     return users
 
+
 def main():
     parser = optparse.OptionParser(sys.argv[0] + ' -c city -o output')
-    parser.add_option('-c', dest='coption', type='string', \
-    help='City for search')
-    parser.add_option('-o', dest='ooption', type='string', \
-    help='Output file in .csv format')
+    parser.add_option('-c', dest='coption', type='string',
+                      help='City for search')
+    parser.add_option('-o', dest='ooption', type='string',
+                      help='Output file in .csv format')
     (options, args) = parser.parse_args()
     if options.coption == None:
         print(parser.usage)
@@ -62,7 +66,7 @@ def main():
     else:
         output_filename = options.ooption
     direction = 'https://github.com/search?q=' + city.replace(' ', '+') +\
-    '&type=Users&utf8=%E2%9C%93'
+        '&type=Users&utf8=%E2%9C%93'
     r = requests.get(direction)
     first_page = r.text
     try:
@@ -73,16 +77,16 @@ def main():
     print('Gathering pages info...')
     urls = []
     for i in range(number_results):
-        link = 'https://github.com/search?p=' + str(i+1) + '&q=' + \
-        city.replace(' ', '+') + '&type=Users&utf8=%E2%9C%93'
+        link = 'https://github.com/search?p=' + str(i + 1) + '&q=' + \
+            city.replace(' ', '+') + '&type=Users&utf8=%E2%9C%93'
         urls.append(link)
-    #Save html found pages
+    # Save html found pages
     htmls = []
     for i, url in enumerate(urls):
         r = requests.get(url)
-        print(str(i+1), '/', str(number_results), sep='')
+        print(str(i + 1), '/', str(number_results), sep='')
         htmls.append(r.text)
-    #String users
+    # String users
     users = find_users(htmls)
     if len(users) == 0:
         print('No results found, exit...')
@@ -95,17 +99,13 @@ def main():
         print(str(cont + 1), '/', str(len(users)), sep='')
         dict_list.append(dicc)
     print('Saving csv file', output_filename)
-    saving_success = False
-    while saving_success == False:
-        try:
-            with open(output_filename, 'w') as f:
-                w = csv.DictWriter(f, dict_list[0].keys())
-                w.writeheader()
-                for dic in dict_list:
-                    w.writerow(dic)
-            saving_success == True
-        except:
-            print('Error saving file, choose other file name')
-            output_filename = input('>> ')
+    try:
+        with open(output_filename, 'w') as f:
+            w = csv.DictWriter(f, dict_list[0].keys())
+            w.writeheader()
+            for dic in dict_list:
+                w.writerow(dic)
+    except:
+        pass
 if __name__ == '__main__':
     main()
